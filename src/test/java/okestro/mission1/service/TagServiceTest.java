84edb457ca8be4f7c81f53e6411ca4b6960b84c5
sami <ddung355@gmail.com>
2025-05-20 14:10:54 +0900

    [Test]: 태그 중복 검사를 수행하는 로직 테스트 코드 추가

--- src/test/java/okestro/mission1/service/TagServiceTest.java
+++ src/test/java/okestro/mission1/service/TagServiceTest.java
@@ -2,7 +2,9 @@ package okestro.mission1.service;
 
 import okestro.mission1.entity.Tag;
 import okestro.mission1.repository.TagRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +33,8 @@ class TagServiceTest {
                         Tag.builder().title("DEV").build(),
                         Tag.builder().title("TEST").build(),
                         Tag.builder().title("BUILD").build(),
-                        Tag.builder().title("PROD").build()
+                        Tag.builder().title("PROD").build(),
+                        Tag.builder().title("DUPLICATE").build()
                 )
         );
     }
@@ -45,4 +48,33 @@ class TagServiceTest {
         assertThat(tags).hasSize(4);
     }
 
+    @Nested
+    class 중복된_태그명이_주어질_때 {
+        //given
+        String duplicateTitle = "DUPLICATE";
+
+        @Test
+        void 중복_검사시_false_가_반환된다() {
+            //when
+            boolean result = tagService.checkTitle(duplicateTitle);
+
+            //then
+            Assertions.assertThat(result).isFalse();
+        }
+    }
+
+    @Nested
+    class 중복되지_않는_태그명이_주어질_때 {
+        //given
+        String originTitle = "DUPLICATE";
+
+        @Test
+        void 중복_검사시_false_가_반환된다() {
+            //when
+            boolean result = tagService.checkTitle(originTitle);
+
+            //then
+            Assertions.assertThat(result).isTrue();
+        }
+    }
 }
