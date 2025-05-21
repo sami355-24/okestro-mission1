ce4d42cd52d4b86d87514e525c177973b85f8311
sami <ddung355@gmail.com>
2025-05-21 14:51:37 +0900

    [Fix]: 더미데이터 생성하는 방법 변경에 의해 동작하지 않던 테스트 코드 수정

---
+++ src/main/java/okestro/mission1/service/VmService.java
@@ -0,0 +1,15 @@
+package okestro.mission1.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = PROTECTED, makeFinal = true)
+public class VmService {
+
+
+}
