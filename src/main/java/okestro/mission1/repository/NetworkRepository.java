3e5de5374717e699557be070ebcb610594dcd98b
sami <ddung355@gmail.com>
2025-06-02 13:36:41 +0900

    [Remove]: 불필요한 import문 삭제

--- src/main/java/okestro/mission1/repository/NetworkRepository.java
+++ src/main/java/okestro/mission1/repository/NetworkRepository.java
@@ -2,8 +2,6 @@ package okestro.mission1.repository;
 
 import okestro.mission1.entity.Network;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
